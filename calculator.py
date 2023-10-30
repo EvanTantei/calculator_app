8225b358188d7f639716ae874bdcd23bba0bd032
EvanTantei <evanlim991@gmail.com>
2023-10-30 22:36:41 +0700

    hello2sys

--- calculator.py
+++ calculator.py
@@ -11,9 +11,88 @@ Window.size = (500, 700)
 Builder.load_file('calc.kv')
 
 class MyLayout(Widget):
+    #clear barfunction
+    def remove(self):
+        available = self.ids.calc_input.text
+        #remove 1letter on the last text
+        available = available[:-1]
+        self.ids.calc_input.text = available
+        #set the default text box when it"s all deleted
+        if available == "":
+            self.ids.calc_input.text = '0'
+
     def clear(self):
         self.ids.calc_input.text = '0'
+
+    def button_press(self, button):
+        #create a variable text calculator
+        available = self.ids.calc_input.text
+
+        if available == '0':
+            self.ids.calc_input.text = ' '
+            self.ids.calc_input.text = f'{button}'
+
+        else:
+            self.ids.calc_input.text = f'{available}{button}' 
     
+    # adding math function
+
+    def math_sign(self, sign):
+        available = self.ids.calc_input.text
+
+        self.ids.calc_input.text = f'{available}{sign}'
+
+
+    def equals(self):
+        available_ess = self.ids.calc_input.text
+        available = self.ids.calc_input.text
+        answer = 0
+        num_list  = available.split('+', '-', '/', '*')
+        sign_list = available.split(num_list)
+        
+        #addition
+        for element in sign_list:
+            if "+" in sign_list:
+                #loop list
+                for element in num_list:
+                    answer = answer + int(element)
+    
+                self.ids.calc_input.text = str(answer)
+
+            elif "-" in available:
+                num_list = available.split("-")
+                 #loop list
+                for element in num_list:
+                    answer = answer - int(element)
+                self.ids.calc_input.text = str(answer)
+
+            elif "x" in available:
+                num_list = available.split("x")
+                #loop list
+                for element in num_list:   
+                    answer = answer * int(element)
+                self.ids.calc_input.text = str(answer)
+
+            elif "/" in available:
+                num_list = available.split("/")
+                #loop list
+                for element in num_list:
+                    answer = answer / int(element)
+                #display the answer into the text box
+                self.ids.calc_input.text = str(answer)
+            
+    def decimal(self):
+        available = self.ids.calc_input.text
+        if "." in available:
+            pass 
+        #add decimal to end of text
+        available = f'{available}.'
+        #display
+        self.ids.calc_input.text = available
+
+
+
+#main system generator
 class calcApp(App):
     def build(self):
         return MyLayout()
